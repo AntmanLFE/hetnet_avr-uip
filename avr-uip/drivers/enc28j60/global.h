aba2d347c0cc3c2969f851a38c90a090dc1a446a
jacopo mondi <mondi@cs.unibo.it>
2011-07-08 09:52:04 +0200

    Indent and replace windows line termination character in enc28j60 driver

--- avr-uip/drivers/enc28j60/global.h
+++ avr-uip/drivers/enc28j60/global.h
@@ -2,7 +2,7 @@
 //*****************************************************************************
 //
 // File Name	: 'global.h'
-// Title		: AVRlib project global include 
+// Title		: AVRlib project global include
 // Author		: Pascal Stang - Copyright (C) 2001-2002
 // Created		: 7/12/2001
 // Revised		: 9/30/2002
@@ -29,6 +29,7 @@
 #include "avrlibtypes.h"
 
 // project/system dependent defines
-#define CYCLES_PER_US ((F_CPU+500000)/1000000) 	// cpu cycles per microsecond
+//#define CYCLES_PER_US ((F_CPU+500000)/1000000) 	// cpu cycles per microsecond
+#define CYCLES_PER_US ((F_CPU)/1000000) 	// cpu cycles per microsecond
 
 #endif
